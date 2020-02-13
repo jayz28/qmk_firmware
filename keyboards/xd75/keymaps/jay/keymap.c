4269b5a81629d40c5f3cfa5e21c9d48a371aad06
Jay Lee <jay@resolutionbio.com>
2020-02-13 10:45:33 -0800

    tweaks

--- keyboards/xd75/keymaps/jay/keymap.c
+++ keyboards/xd75/keymaps/jay/keymap.c
@@ -30,7 +30,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_DV] = LAYOUT_ortho_5x15(
             KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, MO(_SUP), KC_SLCK, MO(_SUP), KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS,
-            KC_CAPS, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_INS, KC_HOME, KC_PGUP, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH,
+            KC_TAB, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_INS, KC_HOME, KC_PGUP, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH,
             KC_TAB, KC_A, KC_O, KC_E, KC_U, KC_I, KC_PSCR, KC_END, KC_PGDN, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS,
             KC_LSFT, KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_LCTL, KC_LALT, LT(_DIR, KC_ESC), KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT,
             MO(_DIR), KC_GRV, KC_BSLS, KC_LEFT, KC_RIGHT, KC_BSPC, KC_DEL, KC_LGUI, KC_ENT, KC_SPC, KC_UP, KC_DOWN, KC_LBRC, KC_RBRC, MO(_QW)),
@@ -90,5 +90,7 @@ void led_set_user(uint8_t usb_led) {
 void keyboard_post_init_user(void) {
     rgblight_enable_noeeprom();
     rgblight_mode(RGBLIGHT_MODE_RAINBOW_MOOD + 2);
+    backlight_enable();
+    backlight_level(2);
 }
 
